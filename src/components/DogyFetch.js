a20e0f12f310e256c77de11938a73b7dfdfafd91
RoxanaCS <roxale.cares@gmail.com>
2018-03-15 12:46:28 -0300

    cambios repo master

---
+++ src/components/DogyFetch.js
@@ -0,0 +1,18 @@
+// constructor({dogy}) {
+//   super();
+//   this.state = {
+//     dogy,
+//     data: null
+//   };
+//   console.log('constructor');
+// }
+// componentWillMount() {
+//   fetch('https://dog.ceo/api/breeds/image/random')
+//     .then(response => {
+//       return response.json();
+//     }).then(data => {
+//       console.log(data);
+//       this.setState({data});
+//       // console.log(dataDog.message);
+//     });
+// }

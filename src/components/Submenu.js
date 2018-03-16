60fdaa5428ff74d453531c06ae5202e0576ee1ac
AGuerrav <aaguerravaldes@gmail.com>
2018-03-15 21:21:44 -0300

    conflictos

--- src/components/Submenu.js
+++ src/components/Submenu.js
@@ -1,22 +1,14 @@
 import React from 'react';
-import AppBar from 'material-ui/AppBar';
-import IconButton from 'material-ui/IconButton';
-import SvgIcon from 'material-ui/SvgIcon';
 
 const SubMenu = () => (
 
   <div>
-        <ul className='navbar'>
-          <li className='links'><i class="material-icons">history</i>Recientes</li>
-          <li className='links'><i class="material-icons">trending_up</i>Populares</li>
-          <li className='login'><i class="material-icons">favorite_border</i>Amados!</li>
-        </ul>
-          </div>
-    // {/* <AppBar
-    //     title="RanDog"
-    //     iconElementRight={<IconButton>
-    //         <History/>
-    //     </IconButton>} */}
-    // />
+    <ul className='navbar'>
+      <li className='links'><i class="material-icons">history</i>Recientes</li>
+      <li className='links'><i class="material-icons">trending_up</i>Populares</li>
+      <li className='login'><i class="material-icons">favorite_border</i>Amados!</li>
+    </ul>
+  </div>
+    
 );
 export default SubMenu;
